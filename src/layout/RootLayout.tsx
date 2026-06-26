f0b2c7bffad5e1293c178a668c7419c94652ccc2
midori-suzune <chuhieu457@gmail.com>
2026-06-26 21:14:57 +0700

    feat : open and close SideMenu.tsx use React context

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,33 +1,34 @@
 import {Outlet} from "react-router-dom";
 import {NavBar} from "../components/NavBar/NavBar.tsx";
-import {Fragment} from "react";
 import {Footer} from "../components/Footer/Footer.tsx";
 import {SideMenu} from "../components/SideMenu/SideMenu.tsx";
+import {SideMenuProvider} from "../context/SideMenuProvider.tsx";
 
 export function RootLayout() {
     return (
-        <div className={"relative"}>
-            {/*
+        <SideMenuProvider>
+            <div className={"relative"}>
+                {/*
                 NavBar use shared for all pages
             */}
-            <nav>
-                <NavBar/>
-            </nav>
-            <main>
-                {/*
+                <nav>
+                    <NavBar/>
+                </nav>
+                <main>
+                    {/*
                     if url is one of url in array
                     the Outlet will be rendered with that url
                     To be precise , Outlet is just a blank in order to child route can be
                     inserted into it's position
                 */}
-                <Outlet/>
-            </main>
-            <SideMenu>
+                    <Outlet/>
+                </main>
 
-            </SideMenu>
-            <footer>
-                <Footer/>
-            </footer>
-        </div>
+                <SideMenu/>
+                <footer>
+                    <Footer/>
+                </footer>
+            </div>
+        </SideMenuProvider>
     );
 }
